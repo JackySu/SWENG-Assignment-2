5d6bbda04a9b15c47345c629557861237a2d4e38
u <hxsushi@126.com>
2022-10-02 19:33:15 +0000

    Add a couple of exception handlers, update unittests

--- calc.py
+++ calc.py
@@ -36,12 +36,12 @@ def split_num_operators(a: str) -> List:
             res.append(a[i])
             i += 1
         else:
-            i += 1
+            raise Exception(f"illegal char '{a[i]}' at index {i}")
 
     return res
 
 
-def operate(operator: str, a: str, b: str) -> None:
+def operate(operator: str, a: str, b: str) -> float:
     a, b = float(a), float(b)
     if operator == '+':
         return a + b
@@ -50,10 +50,13 @@ def operate(operator: str, a: str, b: str) -> None:
     if operator == '*':
         return a * b
     if operator == '/':
-        return a / b
+        try:
+            return a / b
+        except ZeroDivisionError:
+            raise Exception(f"Can not be divided by 0")
 
 
-def calculate() -> int:
+def calculate(result: List, stack: List) -> int:
     operand2, operand1 = result.pop(), result.pop()
     operator = stack.pop()
     ret = operate(operator, operand1, operand2)
@@ -61,7 +64,7 @@ def calculate() -> int:
     return 0
 
 
-def convert_to_postfix(expressions: List) -> None:
+def convert_to_postfix(expressions: List, result: List, stack: List) -> None:
     for i in expressions:
         # add to result if it is a number
         if i.isdigit():
@@ -71,7 +74,7 @@ def convert_to_postfix(expressions: List) -> None:
             # !MARK 1!
             # pop from stack and calculate if it has lower priority
             while priority[stack[-1]] >= priority[i]:
-                calculate()
+                calculate(result, stack)
 
             stack.append(i)
         # push into stack if it is the left bracket
@@ -80,26 +83,32 @@ def convert_to_postfix(expressions: List) -> None:
         # if it is the right bracket then calculate until reaches the left bracket
         elif i == ')':
             while stack[-1] != '(':
-                calculate()
+                calculate(result, stack)
 
             stack.pop()
 
 
-def main():
+def main(s=None) -> float:
     try:
-        expression_list = split_num_operators(input("Enter expression: "))
-        convert_to_postfix(expression_list)
+        # '~' works as an occupant simply does nothing
+        stack = ['~']
+        result = []
+        expression_list = split_num_operators(input("Enter expression: ")) if s is None else split_num_operators(s)
+        convert_to_postfix(expression_list, result, stack)
         while (len(result) > 1):
-            calculate()
+            calculate(result, stack)
+        return result[0]
 
-        print(result[0])
     except IndexError:
         print("Error in expression, please re-input")
+    except Exception as e:
+        print(e)
+    return None
 
 
 if __name__ == "__main__":
     while True:
-        # '~' works as an occupant simply does nothing
-        stack = ['~']
-        result = []
-        main()
+
+        res = main()
+        if res is not None:
+            print(res)
--- test.py
+++ test.py
@@ -1,1 +1,20 @@
+from calc import *
 import unittest
+
+class TestCases(unittest.TestCase):
+
+    def test_split_expression(self):
+        try:
+            self.assertEqual(split_num_operators("11+22-33*44/55"), ["11", "+", "22", "-", "33", "*", "44", "/", "55"])
+        except Exception as e:
+            print(e)
+
+    def test_evaluate(self):
+        try:
+            self.assertEqual(main(s="1+2*3-4/5"), 6.2)
+        except Exception as e:
+            print(e)
+
+
+if __name__ == "__main__":
+    unittest.main()
